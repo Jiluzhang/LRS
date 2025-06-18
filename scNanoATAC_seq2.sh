1ad6c1ab010e5a2555ba39e5e393ccb07b407cc2
Luz <779410241@qq.com>
2025-06-18 17:56:28 +0800

    Update scNanoATAC_seq2.sh

--- scNanoATAC_seq2.sh
+++ scNanoATAC_seq2.sh
@@ -41,6 +41,68 @@ minimap2 -x map-ont -d GRCh38_ONT.mmi /fs/home/jiluzhang/LRS/Homo_sapiens.GRCh38
 #                  - ava-pb/ava-ont - PacBio CLR/Nanopore read overlap
 # -d FILE      dump index to FILE []
 
+
+############################################################################################################################################
+## ./read2frag SRR28246682
+
+sample=$1
+
+echo `date` "......." $sample  start
+
+mkdir $sample
+
+cp /fs/home/jiluzhang/LRS/scNanoATAC_seq2/$sample\_1.fastq.gz .
+
+minimap2 --MD -a -x map-ont -t 8 GRCh38_ONT.mmi $sample\_1.fastq.gz > $sample.sam
+
+samtools view -bS -q 30 -@ 8 $sample.sam > $sample\_Q30.bam 
+samtools sort -@ 8 $sample\_Q30.bam -o $sample\_Q30_sorted.bam
+samtools index -@ 8 $sample\_Q30_sorted.bam
+samtools rmdup -s $sample\_Q30_sorted.bam $sample\_Q30_sorted_rmdup.bam
+samtools index -@ 8 $sample\_Q30_sorted_rmdup.bam
+
+bedtools bamtobed -i $sample\_Q30_sorted_rmdup.bam | awk -vOFS='\t' \
+                                                         '{match($7, /(^[0-9]+)[SH]/, x)
+                                                           lc=x[1]
+                                                           match($7, /([0-9]+)[SH]$/, x) 
+                                                           rc=x[1]
+                                                           if (lc == "") lc=0
+                                                           if (rc == "") rc=0
+                                                           if (lc < fc && rc < fc) print $0}' \
+                                                           fc=150 |\
+                                                    sort -k1,1 -k2,2n | gzip > $sample.bed.gz                  
+zcat $sample.bed.gz | awk -vOFS='\t' '{print "chr"$0}' | bgzip > $sample\_fragments.bed.gz
+tabix $sample\_fragments.bed.gz
+
+zcat $sample\_fragments.bed.gz \
+  | grep -E "^chr[0-9]|^chrX|^chrY" \
+  | bedtools flank -i - -r 0 -l 1 -g hg38.chrom.sizes \
+  | awk -vOFS='\t' '{$6="-"; print $1,$2,$3}' > $sample\_fragments_L.bed
+zcat $sample\_fragments.bed.gz \
+  | grep -E "^chr[0-9]|^chrX|^chrY" \
+  | bedtools flank -i - -r 1 -l 0 -g hg38.chrom.sizes \
+  | awk -vOFS='\t' '{$6="+"; print $1,$2,$3,$4}' > $sample\_fragments_R.bed
+paste $sample\_fragments_L.bed $sample\_fragments_R.bed > $sample\_fragments_L_R.bedpe
+
+rm $sample\_1.fastq.gz $sample.sam $sample\_Q30.bam $sample\_Q30_sorted.bam $sample\_Q30_sorted.bam.bai $sample.bed.gz $sample\_fragments_L.bed $sample\_fragments_R.bed
+
+mv $sample* $sample
+
+echo `date` "......." $sample  done
+############################################################################################################################################
+
+## 10 GM12878 cells
+# SRR28246682  SRR28246693  SRR28246697  SRR28246699  SRR28246704 SRR28246706  SRR28246709  SRR28246712  SRR28246714  SRR28246722
+for srr_id in SRR28246682  SRR28246693  SRR28246697  SRR28246699  SRR28246704 SRR28246706  SRR28246709  SRR28246712  SRR28246714  SRR28246722;do
+    ./read2frag $srr_id >> gm12878_10.log
+done
+
+
+## mouse embryo
+wget -c https://ftp.ensembl.org/pub/release-102/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.primary_assembly.fa.gz
+
+
+
 minimap2 --MD -a -x map-ont -t 8 GRCh38_ONT.mmi SRR28246669_1.fastq.gz > SRR28246669.sam
 # --MD         output the MD tag
 # -a           output in the SAM format (PAF by default)
@@ -51,6 +113,7 @@ samtools sort -@ 8 SRR28246669_Q30.bam -o SRR28246669_Q30_sorted.bam
 samtools index -@ 8 SRR28246669_Q30_sorted.bam
 samtools rmdup -s SRR28246669_Q30_sorted.bam SRR28246669_Q30_sorted_rmdup.bam
 samtools index -@ 8 SRR28246669_Q30_sorted_rmdup.bam
+
 
 # cat SRR28246669_Q30_sorted_rmdup.bam | bamToBed -cigar | awk -vOFS='\t' \
 #                                                          '{match($7, /(^[0-9]+)[SH]/, x)
@@ -103,42 +166,16 @@ zcat SRR28246669_fragments.bed.gz \
 
 paste SRR28246669_fragments_L.bed SRR28246669_fragments_R.bed > SRR28246669_fragments_L_R.bedpe
 
-awk '{if($1=="chr3" && $2>(196082123-100000) && $3<(196082123+100000)) print $0}' human_cCREs.bed | awk '{print $0 "\t" NR-1}' > ccre.bed  
-zcat k562_hq_10_sorted.bed.gz | awk '{if($1=="3" && $2>(196082123-100000) && $3<(196082123+100000)) print "chr"$1 "\t" $2 "\t" $3 "\t" $4}' > fire.bed  
-bedtools intersect -a ccre.bed -b fire.bed -wa -wb -loj | awk '{print $4 "\t" $8}' | uniq > ccre_reads.txt
 
-import pandas as pd
-import numpy as np
-from itertools import combinations
-import igraph as ig
 
-ccre_read = pd.read_table('ccre_reads.txt', header=None)
-ccre_read.columns = ['cre', 'read']
-read_lst = ccre_read['read'].drop_duplicates().values
-read_lst = np.delete(read_lst, np.where(read_lst=='.'))
 
-cre_lst = ccre_read['cre'].drop_duplicates().values
-m = np.zeros([len(cre_lst), len(cre_lst)])
-m = m.astype(int)
 
-ccre_read.drop_duplicates(inplace=True)
 
-for i in read_lst:
-    cre_pair_lst = [list(j) for j in list(combinations(ccre_read[ccre_read['read']==i]['cre'].values, 2))]
-    cre_pair_row = [p[0] for p in cre_pair_lst]
-    cre_pair_col = [p[1] for p in cre_pair_lst]
-    m[cre_pair_row, cre_pair_col] += 1
 
-m = m + m.T
 
-## without edge label
-g = ig.Graph.Weighted_Adjacency(m, mode='undirected')
-g.vs["name"] = cre_lst
-g.vs["label"] = g.vs["name"]
-row_indices, col_indices = np.triu_indices(m.shape[0], k=1)
-g.es["weight"] = [i for i in m[row_indices, col_indices] if i!=0]
-visual_style = {'edge_width':g.es['weight']}
-ig.plot(g, 'TFRC_network.pdf', bbox=(3000, 3000), **visual_style)
+
+
+
 
 
 
