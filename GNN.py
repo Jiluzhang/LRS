76516b2fef8289443a357d1902e61930e6d3036c
Luz <779410241@qq.com>
2025-07-11 17:43:55 +0800

    Update GNN.py

--- GNN.py
+++ GNN.py
@@ -7,7 +7,7 @@ conda install conda-forge::pytorch_geometric
 conda install pytorch::pytorch
 conda install ipython
 
-
+######## self-supervised learning ########
 import torch
 import torch.nn.functional as F
 from torch_geometric.data import Data
@@ -88,6 +88,146 @@ for i in range(all_possible_edges.size(1)):
 # Edge 2-3: Predicted Weight = 0.4432
 # Edge 2-4: Predicted Weight = 0.4350
 # Edge 3-4: Predicted Weight = 0.5492
+
+
+
+######## supervised learning ########
+# conda install conda-forge::scikit-learn
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.data import Data, Batch
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import TransformerConv, global_mean_pool
+from sklearn.metrics import roc_auc_score, average_precision_score
+import numpy as np
+
+from torch_geometric.utils import negative_sampling
+
+def generate_random_graph(num_nodes, num_edges, feat_dim=16):
+    # 生成节点特征和边
+    x = torch.randn(num_nodes, feat_dim)
+    edge_index = torch.randint(0, num_nodes, (2, num_edges))
+    
+    # 生成边标签（正样本=1，负样本=0）
+    edge_label = torch.ones(edge_index.size(1))  # 正样本
+    neg_edges = negative_sampling(edge_index, num_nodes=num_nodes, num_neg_samples=num_edges)  # 负采样
+    full_edge_index = torch.cat([edge_index, neg_edges], dim=1)
+    full_edge_label = torch.cat([edge_label, torch.zeros(neg_edges.size(1))])
+    
+    return Data(x=x, edge_index=edge_index, edge_label=full_edge_label, edge_label_index=full_edge_index)
+
+# 生成5个随机图作为数据集
+dataset = [generate_random_graph(num_nodes=np.random.randint(200, 500),  # 每个图20~50个节点
+                                 num_edges=np.random.randint(3000, 10000)) for _ in range(10)]
+
+# 划分训练集和测试集
+train_dataset = dataset[:8]
+test_dataset = dataset[8:]
+
+class MultiGraphTransformer(nn.Module):
+    def __init__(self, in_dim, hidden_dim, heads=4):
+        super().__init__()
+        self.conv1 = TransformerConv(in_dim, hidden_dim, heads=heads, dropout=0.2)
+        self.conv2 = TransformerConv(hidden_dim*heads, hidden_dim, heads=1, dropout=0.2)
+        self.decoder = nn.Sequential(nn.Linear(2*hidden_dim, hidden_dim),
+                                     nn.ReLU(),
+                                     nn.Linear(hidden_dim, 1))
+    
+    def forward(self, x, edge_index, batch):
+        # 节点编码
+        h = F.relu(self.conv1(x, edge_index))
+        h = F.dropout(h, p=0.2, training=self.training)
+        h = self.conv2(h, edge_index)  # [num_nodes_total, hidden_dim]
+        
+        return h
+    
+    def decode(self, h, edge_index):
+        # 计算节点对(u,v)的特征拼接
+        h_src = h[edge_index[0]]  # 源节点特征
+        h_dst = h[edge_index[1]]  # 目标节点特征
+        edge_feats = torch.cat([h_src, h_dst], dim=-1)
+        return torch.sigmoid(self.decoder(edge_feats)).squeeze()  # 输出概率
+    
+    def loss(self, pred, label):
+        return F.binary_cross_entropy(pred, label)
+
+# 初始化模型和优化器
+model = MultiGraphTransformer(in_dim=16, hidden_dim=32, heads=4)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+# 数据加载器
+train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=2)
+
+def train():
+    model.train()
+    total_loss = 0
+    for batch in train_loader:
+        optimizer.zero_grad()
+        h = model(batch.x, batch.edge_index, batch.batch)
+        pred = model.decode(h, batch.edge_label_index)
+        loss = model.loss(pred, batch.edge_label.float())
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+    return total_loss / len(train_loader)
+
+def test(loader):
+    model.eval()
+    preds, labels = [], []
+    with torch.no_grad():
+        for batch in loader:
+            h = model(batch.x, batch.edge_index, batch.batch)
+            pred = model.decode(h, batch.edge_label_index)
+            preds.append(pred.cpu())
+            labels.append(batch.edge_label.cpu())
+    
+    pred_all = torch.cat(preds).numpy()
+    label_all = torch.cat(labels).numpy()
+    auc = roc_auc_score(label_all, pred_all)
+    ap = average_precision_score(label_all, pred_all)
+    return auc, ap
+
+# 训练循环
+for epoch in range(1, 101):
+    loss = train()
+    train_auc, train_ap = test(train_loader)
+    test_auc, test_ap = test(test_loader)
+    if epoch % 1 == 0:
+        print(f'Epoch: {epoch:03d}, Loss: {loss:.4f}, '
+              f'Train AUC: {train_auc:.4f}, Test AUC: {test_auc:.4f}, '
+              f'Train AP: {train_ap:.4f}, Test AP: {test_ap:.4f}')
+
+
+## GM127878 scATAC-seq data (hg19)
+wget -c https://ftp.ncbi.nlm.nih.gov/geo/samples/GSM2970nnn/GSM2970932/suppl/GSM2970932_sciATAC_GM12878_counts.txt.gz
+
+## GM12878 Hi-C data (GRCh38)
+wget -c https://4dn-open-data-public.s3.amazonaws.com/fourfront-webprod/wfoutput/5809b32e-0aea-4cf5-a174-cf162d591a35/4DNFI9YAVTI1.hic
+
+
+zcat GSM2970932_sciATAC_GM12878_counts.txt.gz | grep TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA | sed 's/_/\t/g' | cut -f 1,2,3,5 | sort -k1,1 -k2,2n > TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA_hg19.txt  # 5658
+zcat GSM2970932_sciATAC_GM12878_counts.txt.gz | grep GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT | sed 's/_/\t/g' | cut -f 1,2,3,5 | sort -k1,1 -k2,2n > GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT_hg19.txt  # 6904
+
+liftOver TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA_hg19.txt hg19ToHg38.over.chain.gz TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA_hg38.txt unmapped.bed && rm unmapped.bed  # 5656
+liftOver GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT_hg19.txt hg19ToHg38.over.chain.gz GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT_hg38.txt unmapped.bed && rm unmapped.bed  # 6904
+
+java -jar juicer_tools_1.19.02.jar dump oe KR ~/scHiC/bulk_hic/loop_subtype/GSE118911_Fulco-2018-mES_combined_30.hic \
+                                              10:69340000:69340000 10:69420000:69420000 BP 10000 | sed '1d' | cut -f 3
+
+bedtools makewindows -g hg38.chrom.sizes -w 1000 > hg38_1kb_bins.bed
+
+bedtools intersect -a hg38_1kb_bins.bed -b TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA_hg38.txt -wa | uniq > TAATGCGCTTGATTGGCGTCAAGATAGTGGCTCTGA_hg38_1kb.bed  # 9592
+bedtools intersect -a hg38_1kb_bins.bed -b GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT_hg38.txt -wa | uniq > GAGATTCCGAACTCGACTTTAATTAGCCCAGGACGT_hg38_1kb.bed  # 11582
+
+
+
+
+
+
+
 
 
 
